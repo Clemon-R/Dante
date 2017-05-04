3d5703aa9a8d89df3f2046759cea70285c80e326
Debrand-Passard <thomas.debrand-passard@epitech.eu>
2017-05-04 16:16:56 +0200

    Finish map

--- generator/src/create_tab.c
+++ generator/src/create_tab.c
@@ -5,7 +5,7 @@
 ** Login   <tdebrand@epitech.net>
 ** 
 ** Started on  Mon May  1 15:31:53 2017 Thomas DEBRAND PASSARD
-** Last update Thu May  4 15:10:00 2017 Thomas DEBRAND PASSARD
+** Last update Thu May  4 16:15:32 2017 Thomas DEBRAND PASSARD
 */
 
 #include "utils.h"
@@ -24,38 +24,22 @@ char	**finish_tab(char **tab, t_map *map)
   map->y = 0;
   tab[0][0] = '*';
   srand(time(NULL));
-  while (map->x + 1 != map->height || map->y + 1 != map->width)
+  while (1 || boucle++)
     {
-      value_rand = rand() % 4;
-      /* my_put_nbr(value_rand);			// */
-      /* my_putstr(" - boucle numéro : ");		// */
-      /* my_put_nbr(boucle);			// */
-      /* my_putchar('\n');				// */
-      /* my_putstr("x = ");			// */
-      /* my_put_nbr(map->x);			// */
-      /* my_putstr(" / y = ");			// */
-      /* my_put_nbr(map->y);			// */
-      /* my_putchar('\n');				// */
-      if (value_rand == 1)
-	tab = if_up(tab, map);
-      else if (value_rand == 2)
-	tab = if_down(tab, map);
-      else if (value_rand == 3)
-	tab = if_right(tab, map);
-      else if (value_rand == 0)
-	tab = if_left(tab, map);
+      (value_rand = rand() % 4) == 1 ? tab = if_up(tab, map) :
+	value_rand == 2 ? tab = if_down(tab, map) :
+	value_rand == 3 ? tab = if_right(tab, map) :
+	value_rand == 0 ? tab = if_left(tab, map) : 0;
       if ((map->x == map->height && map->y == map->width)
 	  || (map->x == map->height && map->y + 1 == map->width)
 	  || (map->x + 1 == map->height && map->y == map->width)
-	  || (map->x + 1 == map->height && map->y + 1 == map->width ))
+	  || (map->x + 1 == map->height && map->y + 1 == map->width))
 	{
+	  tab[map->height][map->width - 1] = ((map->height % 2 == 1) && (map->width % 2 == 1)) ? '*' : 'X';
 	  tab[map->height][map->width] = '*';
 	  break;
 	}
-      //my_put_tab(tab);
-      boucle++;
     }
-  my_put_tab(tab);
   return (tab);
 }
 
