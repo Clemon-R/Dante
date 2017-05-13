06f1ea24810a3a2347fbde755cad827c2b1863de
Raphaël Goulmot <raphael.goulmot@epitech.eu>
2017-05-13 15:18:34 +0200

    Debug tournament

--- tournament/src/resolve.c
+++ tournament/src/resolve.c
@@ -5,7 +5,7 @@
 ** Login   <raphael.goulmot@epitech.net>
 ** 
 ** Started on  Mon May  1 16:36:39 2017 Raphaël Goulmot
-** Last update Sun May  7 16:07:15 2017 Raphaël Goulmot
+** Last update Sat May 13 15:02:58 2017 Raphaël Goulmot
 */
 
 #include "astar.h"
@@ -19,9 +19,9 @@ char	more_short(t_map *map, t_room *first, t_room *second)
 
   size1 = 0;
   if (first)
-    size1 = (map->height - first->y) * (map->width - first->x);
-  size2 = (map->height - second->y) * (map->width - second->x);
-  if (!first || size1 < size2)
+    size1 = (map->height - (first->y + 1)) + (map->width - (first->x + 1));
+  size2 = (map->height - (second->y + 1)) + (map->width - (second->x + 1));
+  if (!first || size1 > size2)
     return (1);
   return (0);
 }
@@ -29,54 +29,68 @@ char	more_short(t_map *map, t_room *first, t_room *second)
 char	check_room(t_map *map, int y, int x)
 {
   if (y >= 0 && y < map->height && x >= 0 && x < map->width
-      && !map->grid[y][x]->blocked && !map->grid[y][x]->visited)
+      && !map->grid[y][x]->blocked && !map->grid[y][x]->visited
+      && !map->grid[y][x]->used)
     return (1);
   return (0);
 }
 
-t_room	*resolve_pos(t_map *map, int y, int x, t_room *old)
+t_room	*resolve_pos(t_map *map, t_room *old)
 {
   int	i;
-  char	offset[] = {0, 1, 1, 0};
+  char	offset[] = {1, 0, 0, 1, -1, 0, 0, -1};
   int	tmp_x;
   int	tmp_y;
   t_room	*current;
 
   i = 0;
   current = 0;
-  while (i < 4)
+  while (i < 8)
     {
-      tmp_y = y + offset[i + 1];
-      tmp_x = x + offset[i];
+      tmp_y = old->y + offset[i + 1];
+      tmp_x = old->x + offset[i];
       if (check_room(map, tmp_y, tmp_x)
 	  && more_short(map, current, map->grid[tmp_y][tmp_x]))
 	current = map->grid[tmp_y][tmp_x];
       i += 2;
     }
-  if (current)
-    current->parent = old;
   return (current);
+}
+
+static t_room	*change_path(t_room **current, t_room *new)
+{
+  t_room	*tmp;
+
+  tmp = *current;
+  (*current)->visited = false;
+  new->parent = (*current)->parent;
+  *current = (*current)->parent;
+  tmp->parent = 0;
+  new->used = true;
+  return (new);
 }
 
 void	resolve(t_map *map)
 {
   t_room	*current;
   t_room	*new;
+  t_room	*tmp;
 
-  new = 0;
-  current = map->start;
-  while (current && map->end != current)
+  new = map->start;
+  while ((current = new) && map->end != new)
     {
-      if (!(new = resolve_pos(map, current->y
-		      , current->x, current)))
+      if (!(new = resolve_pos(map, current)))
 	new = current->parent;
-      if (new)
-	new->visited = true;
+      if (current->parent && current->parent != new
+	  && (tmp = resolve_pos(map, current->parent))
+	  && more_short(map, new, tmp))
+	new = change_path(&current, tmp);
+      if (new && current->parent != new)
+	{
+	  new->visited = true;
+	  new->parent = current;
+	}
       if (current->parent == new)
 	current->parent = 0;
-      current = new;
     }
-  if (current == map->end)
-    map->start->parent = map->start;
-  display_map(map);
 }
--- tournament/src/solve.c
+++ tournament/src/solve.c
@@ -5,7 +5,7 @@
 ** Login   <raphael.goulmot@epitech.net>
 ** 
 ** Started on  Mon May  1 15:46:24 2017 Raphaël Goulmot
-** Last update Sun May  7 16:04:34 2017 Raphaël Goulmot
+** Last update Sat May 13 14:53:35 2017 Raphaël Goulmot
 */
 
 #include "utils.h"
@@ -26,6 +26,7 @@ void    load_line(char *line, t_room **grid_line, int y)
       room = malloc(sizeof(t_room));
       room->y = y;
       room->x = x;
+      room->used = false;
       room->visited = false;
       room->parent = 0;
       room->blocked = *line != '*';
@@ -109,7 +110,9 @@ void	astar(char *file_name)
   int	x;
 
   map = load_file(file_name);
+  map->start->parent = map->start;
   resolve(map);
+  display_map(map);
   y = 0;
   while (map->grid && map->grid[y] && !(x = 0))
     {
